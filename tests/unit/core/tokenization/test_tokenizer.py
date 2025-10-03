1915c5a731bc350b420749c3a45656add1a86c87
pytholic <rajahaseeb147@gmail.com>
2025-10-03 13:52:56 +0900

    [tests](SimpleTokenizer) Added exception tests for encode method

--- tests/unit/core/tokenization/test_tokenizer.py
+++ tests/unit/core/tokenization/test_tokenizer.py
@@ -5,6 +5,7 @@ Created by @pytholic on 2025.09.14
 
 import pytest
 
+from legollm.core.exceptions import TokenizerError
 from legollm.core.tokenization.tokenizer import SimpleTokenizer
 
 
@@ -75,3 +76,27 @@ class TestSimpleTokenizer:
         ids = tokenizer.encode(text)
         assert ids == [1, 2, 7, 3, 8, 4, 9, 9, 5, 6, 10, 0, 11, 10]
         assert tokenizer.decode(ids) == expected_decoded
+
+    def test_encode_raise_error_on_no_vocab(self):
+        """Test the encode method raises an error on no vocabulary."""
+        tokenizer = SimpleTokenizer()
+        with pytest.raises(
+            TokenizerError, match="Cannot encode without vocabulary. Train tokenizer first."
+        ):
+            tokenizer.encode("Hello world! This, is-- a test.")
+
+    def test_encode_raise_error_on_no_unk_token(self):
+        """Test the encode method raises an error on no UNK token."""
+        vocab = {
+            "Hello": 1,
+            "world": 2,
+            "This": 3,
+            "is": 4,
+            "a": 5,
+            "test": 6,
+        }
+        tokenizer = SimpleTokenizer(vocab)
+        with pytest.raises(
+            TokenizerError, match="Vocabulary must contain <|UNK|> for unknown token handling"
+        ):
+            tokenizer.encode("Hello world! This, is-- a test.")
